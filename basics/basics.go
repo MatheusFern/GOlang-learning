da0d7ec1d6e26fff57a4fbce2a43e6534793a473
MatheusFern <jogafacilgameplays@gmail.com>
2020-09-13 19:18:42 -0300

    basics

---
+++ basics/basics.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+/*
+	while
+	i := 0 
+	for i < 10 {
+		fmt.Println("E aew rapaziada", i)
+		i = i + 1
+	}
+*/
+
+// for
+/* for i := 0; i <10; i++ {
+	fmt.Println("E aew rapaziada", i)
+}
+*/
+}
+
